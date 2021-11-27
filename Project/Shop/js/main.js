fd2af99dbaff6f2fae65c6eefce055e7a8e6699e
Ebragim780 <akbrly42@gmail.com>
2021-11-27 11:50:11 +0330

    .

--- Project/Shop/js/main.js
+++ Project/Shop/js/main.js
@@ -1,18 +1,17 @@
+var menu = document.getElementById('menu');
+var exit = document.getElementById('close');
+var check = document.getElementById('check');
+var mobile = document.getElementById('mobile');
 
-    var menu  = document.getElementById('menu');
-    var exit  = document.getElementById('close');
-    var check = document.getElementById('check');
-    var mobile = document.getElementById('mobile');
+menu.onclick = function () {
+    mobile.setAttribute('class', 'list-mobile');
+}
 
-    menu.onclick = function () {
-        mobile.setAttribute('class' , 'list-mobile');
-    }
+exit.onclick = function () {
+    mobile.setAttribute('class', 'navigation');
+}
 
-    exit.onclick = function () {
-        mobile.setAttribute('class' , 'navigation');
-    }
-    
-    check.onclick = function() {
-        document.querySelector('.container').classList.toggle('active');
-        document.querySelector('.side').classList.toggle('active');
-    }
+check.onclick = function () {
+    document.querySelector('.container').classList.toggle('active');
+    document.querySelector('.side').classList.toggle('active');
+}
