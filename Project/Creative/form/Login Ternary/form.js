82145b7b37e184d99c38b13ec087dec5cbfcfa97
Ebrahim <akbrly42@gmail.com>
2022-04-14 09:39:38 +0430

    edit ternary form

--- Project/Creative/form/Login Ternary/form.js
+++ Project/Creative/form/Login Ternary/form.js
@@ -1,4 +1,3 @@
-
 var form = document.getElementsByClassName('form')[0];
 var button = document.getElementById('send');
 
@@ -11,18 +10,18 @@ document.getElementsByClassName('show-signup')[0].addEventListener('click', func
     resetClass(form, 'reset');
     form.classList.add('signup');
     button.innerText = 'Sign Up';
-});
+})
 
 document.getElementsByClassName('show-signin')[0].addEventListener('click', function () {
     resetClass(form, 'signup');
     resetClass(form, 'reset');
     form.classList.add('signin');
     button.innerText = 'Sign In';
-});
+})
 
 document.getElementsByClassName('show-reset')[0].addEventListener('click', function () {
     resetClass(form, 'signup');
     resetClass(form, 'signin');
     form.classList.add('reset');
     button.innerText = 'Reset Password';
-});
+})
