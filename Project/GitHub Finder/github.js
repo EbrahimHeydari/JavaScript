6f8830d04c77f6148da25a8ed2afb3da211dfa2c
Ebrahim <akbrly42@gmail.com>
2023-09-04 11:14:26 +0330

    add URLSearchParams practice

--- Project/GitHub Finder/github.js
+++ Project/GitHub Finder/github.js
@@ -1,23 +1,35 @@
 class Github {
-  constructor() {
-    this.client_id = 'd9308aacf8b204d361fd'
-    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b'
-    this.repos_count = 5
-    this.repos_sort = 'created: asc'
-  }
+	constructor() {
+		this.client_id = 'd9308aacf8b204d361fd'
+		this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b'
+		this.repos_count = 5
+		this.repos_sort = 'created: asc'
+	}
 
-  getUser = async user => {
-    const baseUrl = 'https://api.github.com/users/'
+	getUser = async user => {
+		const baseUrl = 'https://api.github.com/users/'
 
-    const profileResponse = await fetch(`${baseUrl}${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
-    const repoResponse = await fetch(`${baseUrl}${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+		const profileParams = new URLSearchParams({
+			client_id: this.client_id,
+			client_secret: this.client_secret,
+		})
 
-    const profile = await profileResponse.json()
-    const repos = await repoResponse.json()
+		const repoParams = new URLSearchParams({
+			per_page: this.repos_count,
+			sort: this.repos_sort,
+			client_id: this.client_id,
+			client_secret: this.client_secret,
+		})
 
-    return {
-      profile,
-      repos
-    }
-  }
+		const profileResponse = await fetch(`${baseUrl}${user}?${profileParams}`)
+		const repoResponse = await fetch(`${baseUrl}${user}/repos?${repoParams}`)
+
+		const profile = await profileResponse.json()
+		const repos = await repoResponse.json()
+
+		return {
+			profile,
+			repos,
+		}
+	}
 }
